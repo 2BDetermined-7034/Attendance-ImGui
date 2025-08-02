6f58295f7ed9142e19025946e00e5bc279b505b7
Jack Morozov <jack.c.morozov@gmail.com>
2025-08-02 15:41:57 -0700

    ImGui

--- src/attendance/main.cpp
+++ src/attendance/main.cpp
@@ -1,27 +1,58 @@
-#include <SFML/Graphics.hpp>
+#include <glad/glad.h>
+#define GLFW_INCLUDE_NONE
+#include <GLFW/glfw3.h>
+
+#include <imgui/imgui.h>
+#include <imgui/imgui_impl_glfw.h>
+#include <imgui/imgui_impl_opengl3.h>
+
 #include <attendance/Database.hpp>
 #include <iostream>
 
 int main() {
 	using namespace mstd;
 
-	sf::RenderWindow window(sf::VideoMode({800, 600}), "Attendance", sf::Style::Default);
-	window.setVerticalSyncEnabled(true);
-
-	sf::Clock clock;
-	F32 previousTime = 0.0f;
-	while (window.isOpen()) {
-		F32 time = clock.getElapsedTime().asSeconds();
-		F32 deltaTime = time - previousTime;
-		previousTime = time;
-		while (const std::optional event = window.pollEvent()) {
-			if (event->is<sf::Event::Closed>()) {
-				window.close();
-			}
-		}
-
-		window.display();
+	if (!glfwInit()) {
+		std::cerr << errorText << "Could not initialize GLFW" << std::endl;
+		return 1;
 	}
+
+	GLFWwindow* window = glfwCreateWindow(800, 600, "Attendance", nullptr, nullptr);
+
+	glfwMakeContextCurrent(window);
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+		std::cerr << errorText << "Could not initialize GLAD" << std::endl;
+		glfwTerminate();
+		return 1;
+	}
+
+	IMGUI_CHECKVERSION();
+	ImGui::CreateContext();
+	ImGuiIO& io = ImGui::GetIO();
+
+	ImGui_ImplGlfw_InitForOpenGL(window, true);
+	ImGui_ImplOpenGL3_Init();
+
+	// Enable V-Sync
+	glfwSwapInterval(1);
+
+	while (!glfwWindowShouldClose(window)) {
+		glfwPollEvents();
+
+		ImGui_ImplOpenGL3_NewFrame();
+		ImGui_ImplGlfw_NewFrame();
+		ImGui::NewFrame();
+
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		ImGui::ShowDemoWindow();
+		ImGui::Render();
+		ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+
+		glfwSwapBuffers(window);
+	}
+
+	glfwTerminate();
 
 	return 0;
 }
